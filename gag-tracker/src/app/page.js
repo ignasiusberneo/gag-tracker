7f7787e3413c85ff75fca7dd60f78418e24c5dce
ignasiusberneo <ignasiusberneo95@gmail.com>
2025-07-17 05:41:34 +0700

    fix

--- gag-tracker/src/app/page.js
+++ gag-tracker/src/app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
                 "alieninvasion",
               ];
               const filteredWeather = activeWeather.filter((weather) =>
-                specialWeather.includes(weather.item_id)
+                specialWeather.includes(weather.weather_id)
               );
               if (filteredWeather.length > 0) {
                 playSound();
